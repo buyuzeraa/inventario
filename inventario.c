13131d840c91a9f30d24676cee0fe4f080ce2262
Luan Henrique Dos Anjos Barbosa Brito <buyuzeraa@gmail.com>
2026-04-28 12:37:20 +0000

    invetário

---
+++ inventario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+ //declaração de variáveis
+int main (){
+   char produtoA [20] = "router";
+   char produtoB [15] = "ont";
+   unsigned int estoqueA = 1000;
+   unsigned int estoqueB = 500;
+
+   float valorA = 15.50;
+   float valorB = 9.50;
+   
+   unsigned int estoqueMinimoA = 500;
+   unsigned int estoqueMinimoB = 250;
+
+   double valorTotalA;
+   double valorTotalB;
+
+   //exibição de valores
+   printf("Produto %s tem estoque %u e valor unitário é %f \n", router, estoqueA, valorA);
+   printf("Produto %s tem estoque %u e valor unitário é %f \n", ont, estoqueB, valorB);
+   
+   //comparando valores em estoque
+
+}
